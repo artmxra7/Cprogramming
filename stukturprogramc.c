06f1784e15bca99eb3fb83dbfde76b73097f86be
Artmxra7 <artmxarea@gmail.com>
2018-04-25 05:33:17 +0700

    add Struktur

---
+++ stukturprogramc.c
@@ -0,0 +1,11 @@
+/*	Diawali dengan include header library pustaka untuk
+	Memanggil fungsi yang akan digunakan
+	#include <jenisheader>
+*/
+
+#include <stdio.h>
+
+int main (){
+	printf("Hello World");
+	/*return 0;*/
+}
